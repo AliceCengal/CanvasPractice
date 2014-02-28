e600550822ea160ee44fdcf46e180c1834adeb5a
AliceCengal <cengallut@gmail.com>
2014-02-28 17:12:49 -0600

    eh

--- raw.js
+++ raw.js
@@ -6,25 +6,106 @@ var Particle = (function () {
         this.vy = vy;
     }
     Particle.prototype.update = function () {
-        this.xx = this.xx + this.vx;
-        this.yy = this.yy + this.vy;
+        this.xx += this.vx;
+        this.yy += this.vy;
     };
     return Particle;
+})();
+
+var VelocityFieldPoint = (function () {
+    function VelocityFieldPoint(xx, yy, influenceRadius, bulkNormalizingConstant) {
+        this.xx = xx;
+        this.yy = yy;
+        this.influenceRadius = influenceRadius;
+        this.bulkNormalizingConstant = bulkNormalizingConstant;
+        this.vX = 0;
+        this.vY = 0;
+    }
+    VelocityFieldPoint.prototype.updateWithParticle = function (p) {
+        var dist = Math.sqrt(Math.pow(this.xx - p.xx, 2) + Math.pow(this.yy - p.yy, 2));
+        this.vX += (p.vx * Math.exp(-dist));
+        this.vY += (p.vy * Math.exp(-dist));
+    };
+
+    VelocityFieldPoint.prototype.updateFinal = function () {
+        this.vX /= this.bulkNormalizingConstant;
+        this.vY /= this.bulkNormalizingConstant;
+    };
+    return VelocityFieldPoint;
+})();
+
+var VelocityField = (function () {
+    function VelocityField(fieldWidth, fieldHeight, worldWidth, worldHeight, totalParticleCount) {
+        this.fieldWidth = fieldWidth;
+        this.fieldHeight = fieldHeight;
+        this.worldWidth = worldWidth;
+        this.worldHeight = worldHeight;
+        this.totalParticleCount = totalParticleCount;
+        this.fieldPoints = [];
+        this.cellWidth = worldWidth / fieldWidth;
+        this.cellHeight = worldHeight / fieldHeight;
+
+        for (var cellRow = 0; cellRow < fieldHeight; cellRow++) {
+            for (var cellCol = 0; cellCol < fieldWidth; cellCol++) {
+                this.fieldPoints.push(new VelocityFieldPoint(this.cellWidth * (cellCol + 0.5), this.cellHeight * (cellRow + 0.5), worldWidth / 2, 1.01));
+            }
+        }
+    }
+    VelocityField.prototype.updateWithParticle = function (parc) {
+        this.fieldPoints.forEach(function (vp) {
+            vp.updateWithParticle(parc);
+            if (vp.vX > 10) {
+                vp.vX /= 2.0;
+            }
+            if (vp.vY > 10) {
+                vp.vY /= 2.0;
+            }
+        });
+    };
+
+    VelocityField.prototype.influence = function (p) {
+        this.fieldPoints.forEach(function (vp) {
+            //var distX = Math.abs(vp.xx - p.xx);
+            //var distY = Math.abs(vp.yy - p.yy);
+            var dist = Math.sqrt(Math.pow(vp.xx - p.xx, 2) + Math.pow(vp.yy - p.yy, 2));
+
+            var partMag = Math.sqrt(Math.pow(p.vx, 2) + Math.pow(p.vy, 2));
+            var forceMag = Math.sqrt(Math.pow(vp.vX, 2) + Math.pow(vp.vY, 2));
+
+            p.vx = ((vp.vX) * Math.exp(-dist / 1.0)) / (1);
+            p.vy = ((vp.vY) * Math.exp(-dist / 1.0)) / (1);
+            //p.vx += ( vp.vX * Math.exp(-distX) );
+            //p.vy += ( vp.vY * Math.exp(-distY) );
+        });
+    };
+
+    VelocityField.prototype.updateWithTime = function () {
+    };
+
+    VelocityField.prototype.draw = function (ctx) {
+        this.fieldPoints.forEach(function (fp) {
+            ctx.fillStyle = "#dd0000";
+            ctx.fillRect(fp.xx, fp.yy, 5, 5);
+        });
+    };
+    return VelocityField;
 })();
 
 var Simulation = (function () {
     function Simulation(cnv) {
         this.cnv = cnv;
         this.balls = [];
+        this.maxV = 50;
+        this.cycle = 0;
         this.ctx = cnv.getContext('2d');
         this.worldHeight = cnv.height;
         this.worldWidth = cnv.width;
-        this.ballImage = new Image();
-        this.ballImage.src = "http://i.imgur.com/2qjpEPE.png";
 
-        for (var i = 0; i < 50; i++) {
+        for (var i = 0; i < 500; i++) {
             this.balls[i] = new Particle((Math.random() * this.worldWidth), (Math.random() * this.worldHeight), (Math.random() * 10) - 5, (Math.random() * 10) - 5);
         }
+
+        this.field = new VelocityField(30, 30, this.worldWidth, this.worldHeight, 1000);
     }
     Simulation.prototype.loop = function () {
         this.draw();
@@ -33,6 +114,14 @@ var Simulation = (function () {
 
     Simulation.prototype.update = function () {
         var _this = this;
+        this.cycle = (this.cycle + 1) % 10;
+        if (this.cycle === 0) {
+            //console.log("Field Update loop")
+            this.balls.forEach(function (p) {
+                _this.field.updateWithParticle(p);
+                _this.field.influence(p);
+            });
+        }
         this.balls.forEach(function (p) {
             _this.updateBalls(p);
         });
@@ -60,6 +149,14 @@ var Simulation = (function () {
             p.yy = -p.yy;
             p.vy = -p.vy;
         }
+
+        if (Math.abs(p.vx) > this.maxV) {
+            p.vx /= 2;
+        }
+
+        if (Math.abs(p.vy) > this.maxV) {
+            p.vy /= 2;
+        }
     };
 
     Simulation.prototype.clearWindow = function () {
@@ -68,7 +165,8 @@ var Simulation = (function () {
     };
 
     Simulation.prototype.drawBall = function (p) {
-        this.ctx.drawImage(this.ballImage, p.xx, p.yy);
+        this.ctx.fillStyle = '#d0d0d0';
+        this.ctx.fillRect(p.xx, p.yy, 2, 2);
     };
 
     Simulation.prototype.draw = function () {
@@ -77,6 +175,7 @@ var Simulation = (function () {
         this.balls.forEach(function (p) {
             _this.drawBall(p);
         });
+        //this.field.draw(this.ctx);
     };
     return Simulation;
 })();
--- raw.ts
+++ raw.ts
@@ -9,49 +9,163 @@ class Particle {
   }
   
   update() {
-    this.xx = this.xx + this.vx;
-    this.yy = this.yy + this.vy;
+    this.xx += this.vx;
+    this.yy += this.vy;
   }
 }
+
+interface Field {
+  updateWithParticle(p: Particle): void;
+  updateWithTime(): void;
+  influence(p: Particle): void;
+  draw(ctx: CanvasRenderingContext2D): void;
+}
+
+class VelocityFieldPoint {
+
+  vX: number = 0;
+  vY: number = 0;
+
+  constructor(
+      public xx: number,
+      public yy: number,
+      public influenceRadius: number,
+      public bulkNormalizingConstant: number) {
+  }
+
+  updateWithParticle(p: Particle) {
+
+    var dist = Math.sqrt(Math.pow(this.xx - p.xx, 2) + Math.pow(this.yy - p.yy, 2));
+    this.vX += ( p.vx * Math.exp(-dist) );
+    this.vY += ( p.vy * Math.exp(-dist) );
+
+  }
+
+  updateFinal() {
+    this.vX /= this.bulkNormalizingConstant;
+    this.vY /= this.bulkNormalizingConstant;
+  }
+}
+
+class VelocityField {
+
+  fieldPoints: VelocityFieldPoint[] = [];
+  cellHeight: number;
+  cellWidth: number;
+
+  constructor(
+      public fieldWidth: number,
+      public fieldHeight: number,
+      public worldWidth: number,
+      public worldHeight: number,
+      public totalParticleCount: number) {
+
+    this.cellWidth = worldWidth / fieldWidth;
+    this.cellHeight = worldHeight / fieldHeight;
+
+    for (var cellRow = 0; cellRow < fieldHeight; cellRow++) {
+      for (var cellCol = 0; cellCol < fieldWidth; cellCol++) {
+        this.fieldPoints.push(
+            new VelocityFieldPoint(
+                this.cellWidth*(cellCol + 0.5),
+                this.cellHeight*(cellRow + 0.5),
+                worldWidth/2,
+                1.01));
+      }
+    }
+  }
+
+  updateWithParticle(parc: Particle) {
+    this.fieldPoints.forEach((vp) => {
+      vp.updateWithParticle(parc)
+      if (vp.vX > 10) {
+        vp.vX /= 2.0;
+      }
+      if (vp.vY > 10) {
+        vp.vY /= 2.0;
+      }
+    })
+  }
+
+  influence(p: Particle) {
+    this.fieldPoints.forEach((vp) => {
+
+      //var distX = Math.abs(vp.xx - p.xx);
+      //var distY = Math.abs(vp.yy - p.yy);
+      var dist = Math.sqrt(Math.pow(vp.xx - p.xx, 2) + Math.pow(vp.yy - p.yy, 2));
+
+      var partMag = Math.sqrt(Math.pow(p.vx, 2) + Math.pow(p.vy, 2));
+      var forceMag = Math.sqrt(Math.pow(vp.vX, 2) + Math.pow(vp.vY, 2));
+
+      p.vx = ( (vp.vX) * Math.exp(-dist/1.0)) / ( 1 );
+      p.vy = ( (vp.vY) * Math.exp(-dist/1.0)) / ( 1 );
+
+      //p.vx += ( vp.vX * Math.exp(-distX) );
+      //p.vy += ( vp.vY * Math.exp(-distY) );
+
+    });
+  }
+
+  updateWithTime() {}
+
+  draw(ctx: CanvasRenderingContext2D) {
+    this.fieldPoints.forEach((fp) => {
+      ctx.fillStyle = "#dd0000";
+      ctx.fillRect(fp.xx, fp.yy, 5, 5);
+    })
+  }
+
+}
+
+
 
 class Simulation {
 
   ctx: CanvasRenderingContext2D;
-  xx: number;
-  yy: number;
-  
+
   balls: Particle[] = [];
-  
-  ballImage: Image;
-  ballHeight: number;
-  ballWidth: number;
   
   worldWidth: number;
   worldHeight: number;
+
+  field: Field;
+  maxV: number = 50;
 
   constructor(public cnv: HTMLCanvasElement) {
     this.ctx = cnv.getContext('2d');
     this.worldHeight = cnv.height;
     this.worldWidth = cnv.width;
-    this.ballImage = new Image();
-    this.ballImage.src = "http://i.imgur.com/2qjpEPE.png";
     
-    for (var i = 0; i < 50; i++) {
+    for (var i = 0; i < 500; i++) {
       this.balls[i] = new Particle(
           (Math.random()*this.worldWidth),
           (Math.random()*this.worldHeight),
           (Math.random()*10) - 5,
           (Math.random()*10) - 5)
     }
-    
+
+    this.field = new VelocityField(
+        30, 30,
+        this.worldWidth, this.worldHeight,
+        1000);
   }
 
   loop() {
     this.draw();
     this.update();
   }
-    
+
+  cycle: number = 0;
+
   update() {
+    this.cycle = (this.cycle + 1) % 10
+    if (this.cycle === 0) {
+      //console.log("Field Update loop")
+      this.balls.forEach((p: Particle) => {
+        this.field.updateWithParticle(p);
+        this.field.influence(p);
+      });
+    }
     this.balls.forEach((p: Particle) => {
       this.updateBalls(p);
     })
@@ -79,7 +193,14 @@ class Simulation {
       p.yy = -p.yy;
       p.vy = -p.vy;
     }
-    
+
+    if (Math.abs(p.vx) > this.maxV) {
+      p.vx /= 2;
+    }
+
+    if (Math.abs(p.vy) > this.maxV) {
+      p.vy /= 2;
+    }
   }
   
   clearWindow() {
@@ -88,7 +209,8 @@ class Simulation {
   }
   
   drawBall(p: Particle) {
-    this.ctx.drawImage(this.ballImage, p.xx, p.yy)
+    this.ctx.fillStyle = '#d0d0d0'
+    this.ctx.fillRect(p.xx, p.yy, 2, 2);
   } 
 
   draw() {
@@ -96,6 +218,8 @@ class Simulation {
     this.balls.forEach((p: Particle) => {
       this.drawBall(p);
     })
+
+    //this.field.draw(this.ctx);
   }
 
 }
